7563988d0be16b9abff975be113d1219cd19a90a
William Wolf <wwolf@cpan.org>
2013-05-21 01:58:40 +0000

    Import of WWOLF/App-Notes-0.013 from CPAN.
    gitpan-cpan-distribution: App-Notes
    gitpan-cpan-version:      0.013
    gitpan-cpan-path:         WWOLF/App-Notes-0.013.tar.gz
    gitpan-cpan-author:       WWOLF
    gitpan-cpan-maturity:     released

--- lib/App/Notes.pm
+++ lib/App/Notes.pm
@@ -1,6 +1,6 @@
 package App::Notes;
 {
-  $App::Notes::VERSION = '0.012';
+  $App::Notes::VERSION = '0.013';
 }
 
 # ABSTRACT: Simple. Git-based. Notes.
@@ -18,7 +18,7 @@ App::Notes - Simple. Git-based. Notes.
 
 =head1 VERSION
 
-version 0.012
+version 0.013
 
 =head1 SYNOPSIS
 
